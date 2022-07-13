bf6d8e6bc1184fe49751eb9c4995efe6b5a754d3
J Dark <james.dark.95@gmail.com>
2022-07-13 10:40:21 +0000

    removed commented out section

--- solve_3D_heat_transfer.py
+++ solve_3D_heat_transfer.py
@@ -1,29 +1,10 @@
 from fenics import *
 from parameters_3D import my_model
-from context import FESTIM as F
-
 
 my_model.initialise()
 
 T = my_model.T.T
 
-XDMFFile("3D_Results/T.xdmf").write_checkpoint(
+XDMFFile("Results/3D_results/T.xdmf").write_checkpoint(
     T, "T", 0, XDMFFile.Encoding.HDF5, append=False
 )
-
-
-# def run_heat_transfer():
-#     print("Running Heat transfer calculations")
-
-#     my_model.initialise()
-
-#     T = my_model.T.T
-
-#     XDMFFile("3D_Results/T.xdmf").write_checkpoint(
-#         T, "T", 0, XDMFFile.Encoding.HDF5, append=False
-#     )
-#     return T
-
-
-# if __name__ == "__main__":
-#     # run_heat_transfer()
