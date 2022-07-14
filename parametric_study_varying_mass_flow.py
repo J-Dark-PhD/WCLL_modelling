c6ecbd062a692b09ae353a523d982a17f19ccfb7
J Dark <james.dark.95@gmail.com>
2022-07-14 13:28:25 +0000

    updated to work in container and correct folder locations

--- parametric_study_varying_mass_flow.py
+++ parametric_study_varying_mass_flow.py
@@ -1,4 +1,20 @@
 import numpy as np
+from solve_H_transport import run_H_transport
+from fenics import *
+from parameters_3D import (
+    my_model as my_model_3D,
+    convective_flux_fw,
+    convective_flux_bz,
+)
+from parameters_2D import my_model as my_model_2D
+from h_evaluator import (
+    para_h_bz,
+    para_h_fw,
+    para_flow_velocity_bz,
+    para_flow_velocity_fw,
+)
+from temp_3D_slicer import slicer
+import FESTIM as F
 
 no_values = 11
 
@@ -19,8 +35,8 @@ def heat_transfer_3D(T, mass_flow_bz, mass_flow_fw):
         mass_flow_fw (float): mass flow rate in first wall channels (kg s-1)
     """
     print("Running 3D heat transfer simulation")
-    print("BZ mass flow rate =  {}".format(mass_flow_bz))
-    print("FW mass flow rate =  {}".format(mass_flow_fw))
+    print("BZ mass flow rate =  {} kg/s".format(mass_flow_bz))
+    print("FW mass flow rate =  {} kg/s".format(mass_flow_fw))
     print("Bulk coolant temperature = {}K".format(T))
 
     u_bz = para_flow_velocity_bz(mass_flow_bz=mass_flow_bz)
@@ -51,8 +67,8 @@ def temperature_slicer(T, mass_flow_bz, mass_flow_fw):
         mass_flow_fw (float): mass flow rate in first wall channels (kg s-1)
     """
     print("Running 2D slicer")
-    print("BZ mass flow rate =  {}".format(mass_flow_bz))
-    print("FW mass flow rate =  {}".format(mass_flow_fw))
+    print("BZ mass flow rate = {} kg/s".format(mass_flow_bz))
+    print("FW mass flow rate = {} kg/s".format(mass_flow_fw))
     print("Bulk coolant temperature = {}K".format(T))
 
     filename = (
@@ -80,8 +96,8 @@ def h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw):
         mass_flow_fw (float): mass flow rate in first wall channels (kg s-1)
     """
     print("Running FESTIM hydrogen simulation")
-    print("BZ mass flow rate =  {}".format(mass_flow_bz))
-    print("FW mass flow rate =  {}".format(mass_flow_fw))
+    print("BZ mass flow rate = {} kg/s".format(mass_flow_bz))
+    print("FW mass flow rate = {} kg/s".format(mass_flow_fw))
     print("Bulk coolant temperature = {}K".format(T))
 
     T_file = (
@@ -91,7 +107,7 @@ def h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw):
         )
     )
 
-    my_model_2D.T = F.TempFromXDMF(filename=T_file, label="T")
+    my_model_2D.T = F.TemperatureFromXDMF(filename=T_file, label="T")
     folder = (
         folder_results
         + "bz_mass_flow={:.3e}_fw_mass_flow={:.3e}_T={:.1f}K/".format(
@@ -106,55 +122,16 @@ def h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw):
             export.append = False
             export.define_xdmf_file()
 
-    my_model_2D.initialise()
-
-    # project velocity field onto mesh
-    mesh = Mesh()
-    XDMFFile("meshes/mesh_domains_2D.xdmf").read(mesh)
-    V_ele = VectorElement("CG", mesh.ufl_cell(), 2)
-    V_u = FunctionSpace(mesh, V_ele)
-    cfd_folder = "../data/parametric_studies/varying_h_coeff/"
-    mesh_cfd = cfd_folder + "u_2D.xdmf"
-    u = Function(V_u, name="velocity")
-    XDMFFile(mesh_cfd).read_checkpoint(u, "u", -1)
-
-    # modify the form F
-    id_flow = id_lipb
-    test_function_solute = my_model_2D.h_transport_problem.mobile.test_function
-    solute = my_model_2D.h_transport_problem.mobile.solution
-
-    dx = my_model_2D.mesh.dx
-    S_lipb = S_0_lipb * exp(-E_S_lipb / F.k_B / my_model_2D.T.T)
-    my_model_2D.h_transport_problem.F += inner(
-        dot(grad(S_lipb * solute), u), test_function_solute
-    ) * dx(id_flow)
-
-    my_model_2D.run()
+    run_H_transport(my_model_2D)
 
 
 if __name__ == "__main__":
-    from fenics import *
-    from parameters_3D import (
-        my_model as my_model_3D,
-        convective_flux_fw,
-        convective_flux_bz,
-    )
-    from h_evaluator import (
-        para_h_bz,
-        para_h_fw,
-        para_flow_velocity_bz,
-        para_flow_velocity_fw,
-    )
-    from temp_3D_slicer import slicer
-    from parameters_2D import my_model as my_model_2D, id_lipb
-    from properties import S_0_lipb, E_S_lipb
-    from context import FESTIM as F
 
     folder_temperature_fields_3D = (
-        "../data/parametric_studies/varying_h_coeff/3D_temp_fields/"
+        "Results/parametric_studies/varying_h_coeff/3D_temp_fields/"
     )
-    folder_2D_slices = "../data/parametric_studies/varying_h_coeff/2D_slices/"
-    folder_results = "../data/parametric_studies/varying_h_coeff/results/"
+    folder_2D_slices = "Results/parametric_studies/varying_h_coeff/2D_slices/"
+    folder_results = "Results/parametric_studies/varying_h_coeff/results/"
 
     # varying both mass flow rates by same value
     for mass_flow_bz, mass_flow_fw in zip(mass_flow_range_bz, mass_flow_range_fw):
@@ -164,16 +141,16 @@ if __name__ == "__main__":
             h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw)
 
     # varying mass flow rates individually
-    for mass_flow_bz in mass_flow_range_bz:
-        mass_flow_fw = 0.63189
-        for T in temp_range:
-            heat_transfer_3D(T, mass_flow_bz, mass_flow_fw)
-            temperature_slicer(T, mass_flow_bz, mass_flow_fw)
-            h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw)
+    # for mass_flow_bz in mass_flow_range_bz:
+    #     mass_flow_fw = 0.63189
+    #     for T in temp_range:
+    #         heat_transfer_3D(T, mass_flow_bz, mass_flow_fw)
+    #         temperature_slicer(T, mass_flow_bz, mass_flow_fw)
+    #         h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw)
 
-    for mass_flow_fw in mass_flow_range_fw:
-        mass_flow_bz = 0.85491
-        for T in temp_range:
-            heat_transfer_3D(T, mass_flow_bz, mass_flow_fw)
-            temperature_slicer(T, mass_flow_bz, mass_flow_fw)
-            h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw)
+    # for mass_flow_fw in mass_flow_range_fw:
+    #     mass_flow_bz = 0.85491
+    #     for T in temp_range:
+    #         heat_transfer_3D(T, mass_flow_bz, mass_flow_fw)
+    #         temperature_slicer(T, mass_flow_bz, mass_flow_fw)
+    #         h_transport_sim_FESTIM(T, mass_flow_bz, mass_flow_fw)
