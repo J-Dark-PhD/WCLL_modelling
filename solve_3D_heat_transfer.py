b81991e638b6dd1a26cf1304b940a3a94ee72a64
J Dark <james.dark.95@gmail.com>
2023-05-18 18:31:27 +0000

    option for creating submesh of T field

--- solve_3D_heat_transfer.py
+++ solve_3D_heat_transfer.py
@@ -36,10 +36,35 @@ def dense_3D_case():
         T, "T", 0, XDMFFile.Encoding.HDF5, append=False
     )
 
+def standard_3D_case_submesh():
+    my_model.initialise()
+
+    T = my_model.T.T
+
+    mesh_folder = "meshes/"
+    mesh_full = F.MeshFromXDMF(
+        volume_file=mesh_folder + "mesh_domains_3D.xdmf",
+        boundary_file=mesh_folder + "mesh_boundaries_3D.xdmf",
+    )
+    id_lipb = 6
+    mesh_sub = SubMesh(mesh_full.mesh, mesh_full.volume_markers, id_lipb)
+    XDMFFile("meshes/submesh_3D.xdmf").write(mesh_sub)
+
+    print("Projecting temperature field onto mesh")
+    V_CG1_sub = FunctionSpace(mesh_sub, "CG", 1)
+    T.set_allow_extrapolation(True)
+    T_sub = project(T, V_CG1_sub)
+
+    XDMFFile("Results/3D_results/T_3D_sub.xdmf").write_checkpoint(
+        T_sub, "T", 0, XDMFFile.Encoding.HDF5, append=False
+    )
+
 
 if __name__ == "__main__":
     # standard case
     # standard_3D_case()
 
     # More dense mesh, resulting in detailed temperature field
-    dense_3D_case()
+    # dense_3D_case()
+
+    standard_3D_case_submesh()
